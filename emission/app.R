93f61f7722f154012f64663b8538f0da906f5d10
xrander <olamideadu@gmail.com>
2023-12-16 12:44:37 +0100

    first part of project completed - interactive emission data plot

--- emission/app.R
+++ emission/app.R
@@ -1,44 +1,102 @@
-library(shiny)
+library("shiny")
+library("shinythemes")
 library("tidyverse")
 library("janitor")
 
+ghg_data_long <- read_csv("ghg_pivot_longer.csv")
+
+ghg_data_long <- ghg_data_long %>% 
+  select(2:7)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "/bootstrap.min.css")
+  ),
+  # Application title
+  titlePanel("Global Emissions Explorer: Interactive Analysis of Greenhouse Gas Trends (1990-2020)"),
+  hr(),
+  selectInput("ghg", "Greenhouse gas",
+              choices = unique(ghg_data_long$gas),
+              multiple = T),
+  selectInput("country", "Select Region or Country",
+              choices = unique(ghg_data_long$region),
+              multiple = T),
+  tableOutput("total_emission"),
+  # Sidebar with a slider input for number of bins 
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      sliderInput("years",
+                  "Year range:",
+                  min = min(ghg_data_long$year),
+                  max = max(ghg_data_long$year),
+                  value = c(1990, 2020))
+      ),
+    # Print total
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("line_plot"),
+      plotOutput("area_plot"),
+      plotOutput("bar_plot")
     )
-)
+  )
+)  
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  output$total_emission <- renderTable({
+    ghg_data_long %>% 
+      filter(gas %in% input$ghg & region %in% input$country) %>% 
+      filter(year >= input$years[1] & year <= input$years[2]) %>% 
+      group_by(region, gas) %>%
+      summarize(total = sum(value)) %>% 
+      pivot_wider(names_from = gas,
+                  values_from = total) %>% 
+      rowwise() %>% 
+      mutate(emission_total = sum(c_across(-1), na.rm = T))
+  })
+  
+  plot_object <- reactive({
+    req(input$ghg, input$country, input$years)
+    
+    ghg_data_long %>% 
+      filter(gas == input$ghg & region == input$country) %>% 
+      filter(year >= input$years[1] & year <= input$years[2]) %>% 
+      ggplot(aes(year, value))
+  })
+  
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$line_plot <- renderPlot({
+   plot_object() +
+      geom_line(aes(col = region, lty = gas))+
+      labs(y = "Emission (kilitone)",
+           x = "year")+
+      theme_minimal()+
+      theme(legend.position = "right")
+     }, res = 96)
+  
+  output$area_plot <- renderPlot({
+    plot_object() +
+      geom_area(aes(fill = region), alpha = 0.6) +
+      labs(y = "Emission (kilitone)",
+           x = "year") +
+      theme_minimal() +
+      theme(legend.position = "top")+
+      facet_wrap(~gas, scales = "free_y")
+    }, res = 96)
+  
+  output$bar_plot <- renderPlot({
+    plot_object() +
+      geom_col(aes(fill = region), position = "dodge") +
+      labs(y = "Emission (kilitone)",
+           x = "year") +
+      theme_minimal() +
+      theme(legend.position = "top")+
+      facet_wrap(~gas, scales = "free_y")
+      }, res = 96)
 }
 
 # Run the application 
--- ghg.r
+++ ghg.r
@@ -2,10 +2,13 @@ library("tidyverse")
 library("janitor")
 
 files <- list.files(pattern = "\\.csv$", full.names = T)
+files <- files[-1]
+
 
 ghg_data <- map_df(files, read_csv) %>% 
   clean_names() %>% 
   select(-country_or_area_code)
+
 
 skimr::skim(ghg_data)
 
@@ -18,7 +21,7 @@ ghg_data %>%
   theme_light()
   #theme(legend.position = "none")
 
-ghg_data <- ghg_data %>%
+ghg_data_long <- ghg_data %>%
   group_by(series_code, country_or_area) %>% 
   arrange(country_or_area, wt = year,
           .by_group = T) %>%
@@ -34,5 +37,25 @@ ghg_data <- ghg_data %>%
          "lagged_value" = lagged_value_value) %>% 
   mutate(percent_change = (value-lagged_value)/lagged_value * 100) %>% 
   ungroup() %>% 
-  mutate_if(is.character, factor)
+  mutate_if(is.character, factor) 
+
+write.csv(ghg_data_long, "emission/ghg_pivot_longer.csv")
+
+
+ghg_data_wide <- ghg_data_long %>% 
+  select(gas, region, year, value) %>% 
+  pivot_wider(names_from = year,
+              values_from = c(value),
+              values_fn = mean)
+
+
+ghg_data_long %>% 
+  filter(gas %in% c("N20", "CO2", "MIX", "CH4") & region %in% c("United States of America", "United Kingdom")) %>% 
+  filter(year >= 1999 & year <= 2019) %>% 
+  group_by(region, gas) %>% 
+  summarize(total = sum(value)) %>% 
+  pivot_wider(names_from = gas,
+              values_from = total) %>% 
+  rowwise() %>% 
+  mutate(emistotal = sum(c_across(-1), na.rm = T))
 
