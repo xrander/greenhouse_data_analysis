ee300e2b52e043e1daf1b2b29487eab5608f9206
xrander <olamideadu@gmail.com>
2023-12-17 22:53:31 +0100

    themes updated to cerulean

--- emission/app.R
+++ emission/app.R
@@ -9,41 +9,70 @@ ghg_data_long <- ghg_data_long %>%
   select(2:7)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "/bootstrap.min.css")
-  ),
-  # Application title
-  titlePanel("Global Emissions Explorer: Interactive Analysis of Greenhouse Gas Trends (1990-2020)"),
-  hr(),
-  selectInput("ghg", "Greenhouse gas",
-              choices = unique(ghg_data_long$gas),
-              multiple = T),
-  selectInput("country", "Select Region or Country",
-              choices = unique(ghg_data_long$region),
-              multiple = T),
-  tableOutput("total_emission"),
-  # Sidebar with a slider input for number of bins 
-  
-  sidebarLayout(
-    
-    sidebarPanel(
-      sliderInput("years",
-                  "Year range:",
-                  min = min(ghg_data_long$year),
-                  max = max(ghg_data_long$year),
-                  value = c(1990, 2020))
-      ),
-    # Print total
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("line_plot"),
-      plotOutput("area_plot"),
-      plotOutput("bar_plot")
-    )
-  )
-)  
+ui <-
+  navbarPage("Climate Insights: Global Greenhouse Gas Emissions Tracker (1990-2020)",
+             theme = shinytheme("cerulean"),
+             
+             # first tab
+             tabPanel("Dashboard",
+                      fluidPage(
+                        # Application title
+                        titlePanel("Interactive Emission Board"),
+                        hr(),
+                        selectInput("ghg", "Greenhouse gas",
+                                    choices = unique(ghg_data_long$gas),
+                                    multiple = T),
+                        selectInput("country", "Select Region or Country",
+                                    choices = unique(ghg_data_long$region),
+                                    multiple = T),
+                        tableOutput("total_emission"),
+                        # Sidebar with a slider input for number of bins 
+                        sidebarLayout(
+                          sidebarPanel(
+                            sliderInput("years",
+                                        "Year range:",
+                                        min = min(ghg_data_long$year),
+                                        max = max(ghg_data_long$year),
+                                        value = c(1990, 2020)),
+                            checkboxGroupInput("plot_type", "Select Plot Type",
+                                               choices = c("line", "area", "bar"),
+                                               selected = "line")
+                            ),
+                          # Print total
+                          # Show a plot of the generated distribution
+                          mainPanel(
+                            plotOutput("line_plot"),
+                            plotOutput("area_plot"),                   
+                            plotOutput("bar_plot")
+                            )
+                          )
+                        )
+                      ),
+             
+             # second tab
+             tabPanel("Comparison Tool",
+                      fluidPage(
+                        titlePanel("Country and Gas Emission Comparison")
+                        )
+                      ),
+             
+             # third tab
+             tabPanel("Predictive Modeling",
+                      fluidPage(
+                        titlePanel("Future Emission Forecast: Predictive Insights")
+                        )
+                      ),
+             
+             # fourth tab
+             tabPanel("Scenario Analysis"),
+             
+             # fifth tab
+             tabPanel("Emissions Heatmap"),
+             
+             # sixth tab
+             tabPanel("Correlation Analysis")
+             )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -98,6 +127,5 @@ server <- function(input, output) {
       facet_wrap(~gas, scales = "free_y")
       }, res = 96)
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
